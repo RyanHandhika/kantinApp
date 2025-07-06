ee6d096fecd6d7dd975ae81a838cebf41f746d8f
DzakyLLL <jajaaa050206@gmail.com>
2025-07-06 16:00:14 +0700

    membuat koneksi database, menampilkan database data transaksi

--- lib/java_kantin.sql
+++ lib/java_kantin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 04, 2025 at 10:27 AM
+-- Generation Time: Jul 06, 2025 at 09:47 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,17 +24,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dailyreports`
+-- Table structure for table `kategori_menu`
 --
 
-CREATE TABLE `dailyreports` (
+CREATE TABLE `kategori_menu` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kategori_menu`
+--
+
+INSERT INTO `kategori_menu` (`id_kategori`, `nama_kategori`, `created_at`, `update_at`) VALUES
+(1, 'Makanan', '2025-07-04 16:42:52', '2025-07-04 16:42:52'),
+(2, 'Minuman', '2025-07-04 16:42:52', '2025-07-04 16:42:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `laporan`
+--
+
+CREATE TABLE `laporan` (
   `id` int(11) NOT NULL,
+  `kode_menu` varchar(10) NOT NULL,
   `tanggal` date NOT NULL,
-  `total_pendapatan` decimal(12,2) NOT NULL,
+  `total_pendapatan` decimal(12,3) NOT NULL,
   `total_transaksi` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `laporan`
+--
+
+INSERT INTO `laporan` (`id`, `kode_menu`, `tanggal`, `total_pendapatan`, `total_transaksi`, `created_at`, `update_at`) VALUES
+(1, 'M001', '2025-07-05', 51.000, 3, '2025-07-04 17:14:57', '2025-07-04 17:14:57');
 
 -- --------------------------------------------------------
 
@@ -44,63 +73,68 @@ CREATE TABLE `dailyreports` (
 
 CREATE TABLE `menu` (
   `kode_menu` varchar(10) NOT NULL,
-  `category_id` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
   `nama_menu` varchar(100) NOT NULL,
   `deskripsi` text NOT NULL,
-  `harga` decimal(10,0) NOT NULL,
+  `harga` decimal(10,3) NOT NULL,
   `stok` int(11) NOT NULL,
   `gambar` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`kode_menu`, `id_kategori`, `nama_menu`, `deskripsi`, `harga`, `stok`, `gambar`, `created_at`, `update_at`) VALUES
+('M001', 1, 'Nasi Goreng', 'Nasi yang digoreng', 15.000, 100, 'nasiGoreng.png', '2025-07-04 17:02:23', '2025-07-04 17:02:23'),
+('M002', 2, 'Juice Orange', 'Minuman segar yang bersumber dari sari buah jeruk', 7.000, 50, 'juiceOrange.png', '2025-07-04 17:02:23', '2025-07-04 17:02:23');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `menucategories`
+-- Table structure for table `pelanggan`
 --
 
-CREATE TABLE `menucategories` (
-  `category_id` int(11) NOT NULL,
-  `nama_kategori` varchar(50) NOT NULL,
-  `deskripsi` text NOT NULL,
+CREATE TABLE `pelanggan` (
+  `kode_pelanggan` varchar(10) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `telp` varchar(13) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`kode_pelanggan`, `nama`, `telp`, `alamat`) VALUES
+('P001', 'Badrul', '085543213456', 'JL. Soedirman No.01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `kode_transaksi` varchar(10) NOT NULL,
+  `kode_menu` varchar(50) NOT NULL,
+  `metode_pembayaran` enum('tunai','saldo') NOT NULL,
+  `tanggal_transaksi` datetime NOT NULL,
+  `kode_pelanggan` varchar(10) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total` decimal(10,3) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `transactiondetails`
+-- Dumping data for table `transaksi`
 --
 
-CREATE TABLE `transactiondetails` (
-  `id` int(11) NOT NULL,
-  `transaction_id` varchar(10) NOT NULL,
-  `menu_id` varchar(10) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `harga_satuan` decimal(10,2) NOT NULL,
-  `subtotal` decimal(10,2) NOT NULL,
-  `catatan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transactions`
---
-
-CREATE TABLE `transactions` (
-  `kode_transaksi` varchar(10) NOT NULL,
-  `total_harga` decimal(10,2) NOT NULL,
-  `metode_pembayaran` enum('tunai','saldo') NOT NULL,
-  `tanggal_transaksi` datetime NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `nama_pelanggan` varchar(100) NOT NULL,
-  `menu` varchar(100) NOT NULL,
-  `jumlah` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `transaksi` (`kode_transaksi`, `kode_menu`, `metode_pembayaran`, `tanggal_transaksi`, `kode_pelanggan`, `jumlah`, `total`, `created_at`, `update_at`) VALUES
+('T001', 'M001', 'tunai', '2025-07-04 19:27:22', 'P001', 2, 30.000, '2025-07-04 17:27:58', '2025-07-04 17:27:58');
 
 -- --------------------------------------------------------
 
@@ -120,41 +154,49 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `nama_kantin`, `email`, `role`, `created_at`, `update_at`) VALUES
+(1, 'admin', 'admin123', 'admin kantin', 'admin@gmail.com', 'admin', '2025-07-04 17:17:09', '2025-07-04 17:17:09');
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `dailyreports`
+-- Indexes for table `kategori_menu`
 --
-ALTER TABLE `dailyreports`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `kategori_menu`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `laporan`
+--
+ALTER TABLE `laporan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kode_menu` (`kode_menu`);
 
 --
 -- Indexes for table `menu`
 --
 ALTER TABLE `menu`
   ADD PRIMARY KEY (`kode_menu`),
-  ADD KEY `category_id` (`category_id`);
+  ADD KEY `category_id` (`id_kategori`);
 
 --
--- Indexes for table `menucategories`
+-- Indexes for table `pelanggan`
 --
-ALTER TABLE `menucategories`
-  ADD PRIMARY KEY (`category_id`);
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`kode_pelanggan`);
 
 --
--- Indexes for table `transactiondetails`
+-- Indexes for table `transaksi`
 --
-ALTER TABLE `transactiondetails`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `transaction_id` (`transaction_id`),
-  ADD KEY `product_id` (`menu_id`);
-
---
--- Indexes for table `transactions`
---
-ALTER TABLE `transactions`
-  ADD PRIMARY KEY (`kode_transaksi`);
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`kode_transaksi`),
+  ADD KEY `kode_menu` (`kode_menu`),
+  ADD KEY `id_pelanggan` (`kode_pelanggan`);
 
 --
 -- Indexes for table `users`
@@ -168,45 +210,45 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `dailyreports`
+-- AUTO_INCREMENT for table `kategori_menu`
 --
-ALTER TABLE `dailyreports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `kategori_menu`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `menucategories`
+-- AUTO_INCREMENT for table `laporan`
 --
-ALTER TABLE `menucategories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `transactiondetails`
---
-ALTER TABLE `transactiondetails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `laporan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `laporan`
+--
+ALTER TABLE `laporan`
+  ADD CONSTRAINT `laporan_ibfk_1` FOREIGN KEY (`kode_menu`) REFERENCES `menu` (`kode_menu`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `menu`
 --
 ALTER TABLE `menu`
-  ADD CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `menucategories` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori_menu` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `transactiondetails`
+-- Constraints for table `transaksi`
 --
-ALTER TABLE `transactiondetails`
-  ADD CONSTRAINT `transactiondetails_ibfk_1` FOREIGN KEY (`transaction_id`) REFERENCES `transactions` (`kode_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transactiondetails_ibfk_2` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`kode_menu`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`kode_pelanggan`) REFERENCES `pelanggan` (`kode_pelanggan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`kode_menu`) REFERENCES `menu` (`kode_menu`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
